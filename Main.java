6bd7b4568d86b9e8777930c0395ced112ed72fb3
Vu Nguyen <vunguyen01202009@gmail.com>
2025-09-16 21:46:03 +0000

    bruh

--- Main.java
+++ Main.java
@@ -6,27 +6,61 @@ public class Main
     {
         // You can optionally use the Scanner for user input
         Scanner scan = new Scanner(System.in);
-        
+        /*
         int numOfNums;
         System.out.println("How many random numbers do you want to see (must be at least 1)?");
         numOfNums = scan.nextInt();
         printRandom(numOfNums);
+        
+        double x1;
+        System.out.println("Enter the first x-coordinate:");
+        x1 = scan.nextDouble();
+        double x2;
+        System.out.println("Enter the second x-coordinate:");
+        x2 = scan.nextDouble();
+        double y1;
+        System.out.println("Enter the first y-coordinate:");
+        y1 = scan.nextDouble();
+        double y2;
+        System.out.println("Enter the second y-coordinate:");
+        y2 = scan.nextDouble();
+        System.out.println("Slope: " + calcSlope(x1, x2, y1, y2));
+        */
+        System.out.println("Enter two doubles:");
+        System.out.print('>');
+        double 1st;
+        1st = scan.nextDouble();
+        System.out.print("\n>");
+        double 2nd;
+        2nd = scan.nextDouble();
+        System.out.print("\nDistance: " + roundedDist(1st, 2nd));
+
     }
-    
-    public static void printRandom3(int num)
-    {
-        // Do problem 1 here
-    }
-    
-    public static double calcSlope(double x1, double x2, double y1, double y2)
-    {
-        // Do problem 2 here; replace code below
-        return 0.0;
-    }
-    
     public static int roundedDist(double a, double b)
     {
-        // Do problem 3 here; replace code below
-        return 0;
+        int dist = (int)(Math.abs(a - b) + 0.5);
+        return dist;
     }
+    
+    public static void printRandom(int num)
+    {
+        System.out.println("Enter a positive integer:\n" + num);
+        System.out.println("Random numbers:");
+        for(int i = 0 ; i < 3 ; i++)
+        {
+            System.out.println((int)(Math.random() * (num + 1) + 2));
+        }
+        return;
+
+    }
+    
+    public static double calcSlope(double x1, double y1, double x2, double y2)
+    {
+        
+        double slope = (y1 + y2) / (x1 + x2);
+        
+        return slope;
+    }
+    
+    
 }
